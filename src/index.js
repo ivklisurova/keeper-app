b2522ccbd834e9509013c11783512d9e76c90ffd
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2022-04-18 15:34:41 +0300

    deploy

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBS_t5OwcYlcfVO3f-U7nbj6BTFwsQyIMI",
-    authDomain: "keeper-e125a.firebaseapp.com",
-    projectId: "keeper-e125a",
-    storageBucket: "keeper-e125a.appspot.com",
-    messagingSenderId: "679198465253",
-    appId: "1:679198465253:web:ff9ed5fa795cc49021c80c",
+    apiKey: process.env["REACT_APP_FIREBASE_API_KEY"],
+    authDomain: process.env["REACT_APP_FIREBASE_AUTH_DOMAIN"],
+    projectId: process.env["REACT_APP_FIREBASE_PROJECT_ID"],
+    storageBucket: process.env["REACT_APP_FIREBASE_STORAGE_BUCKET"],
+    messagingSenderId: process.env["REACT_APP_FIREBASE_MESSAGING_SENDER_ID"],
+    appId: process.env["REACT_APP_FIREBASE_APP_ID"],
     measurementId: "G-YH4K5LD6GP"
 };
 
